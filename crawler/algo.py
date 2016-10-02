9282e7efc03717606877b153157671eee705026e
nodeproject11 <adarshs@iitk.ac.in>
2016-10-02 12:55:03 +0530

    Bugs Fixed

--- crawler/algo.py
+++ crawler/algo.py
@@ -2,16 +2,16 @@ from crawler.models import *
 from datetime import datetime
 
 def nominalize(company_a, company_list):
-    model_objects = post_info.objects.get(company=company_a)
+    model_objects = post_info.objects.filter(company=company_a)
     total = 0
-    nominalized_likes = {company_a:{:}}
+    nominalized_likes = {company_a:{}}
     for a in model_objects:
         total = a.no_of_likes + total
         nominalized_likes[company_a][a.id] = a.no_of_likes
         mean1 = total/float(len(model_objects))
     for b in company_list:
-        nominalized_likes[b] = {:}
-        model_objects = post_info.objects.get(company=b)
+        nominalized_likes[b] = {}
+        model_objects = post_info.objects.filter(company=b)
         total_l= 0
         for a in model_objects:
             total_l = a.no_of_likes + total_l
@@ -28,8 +28,8 @@ def predict(nominalized_likes, hashtag, company_list):
     model_objects = post_info.objects.all()
     days = 0
     for b in model_objects:
-        category_list = b.category(',')
-        if hastag in category_list:
+        category_list = b.category.split(',')
+        if hashtag in category_list:
             sigma_wn = sigma_wn + nominalized_likes[b.company][b.id]
             total_weight = total_weight + 2
             days = days + (2*(now.day - b.time.day))
--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from django.shortcuts import redirect
+
+
+
+from datetime import datetime 
+from crawler.algo import * 
+from crawler.crawl import * 
+
+def main_page(request):
+	projected_likes = 0
+	projected_days = 0
+	mean_like=0
+	mean_days=0
+	print "LLL"
+	# company_url = "https://graph.facebook.com/antaragni.iitk/posts/?key=value&access_token=176565146081574|9f55220446aa4c2d44560f2ebde2430b"
+	# crawl(company_url)
+	list_companies = ["antaragni","ritambhara"]
+	company_a = request.POST.get('company',False)
+	hashtag = request.POST.get('hashtag',False)
+	if company_a != False:
+		print "post successful"
+		nominalized_likes = nominalize(company_a,list_companies)
+		mean_like,mean_days = predict(nominalized_likes,hashtag,list_companies)
+
+	projected_likes = int(mean_like)
+	projected_days = int(mean_days)
+
+
+
+
+
+	return render(request,'index.html',{'projected_likes':projected_likes})
